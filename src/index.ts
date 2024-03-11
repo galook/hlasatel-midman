23dfa9e44bcf59a6e6458b756d14a076f60fa6bb
Matej Holubec <matej.holubec@gmail.com>
2024-03-11 14:27:06 +0100

    feat: prototype

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,33 @@
-console.log("Hello world!");
+import { WebSocket } from "ws";
+import sound from "play-sound";
+import * as fs from "fs";
+
+const ws = new WebSocket("ws://localhost:8085");
+const player = sound();
+
+ws.on("open", () => {
+  console.log("Connected to server");
+  ws.send("con");
+});
+
+ws.on("message", (data) => {
+ let parsedData;
+  try {
+    parsedData = JSON.parse(data.toString());
+    
+  } catch (e) {
+    console.log('err parsing', e);
+    return;
+  }
+  if(parsedData.action === 'go'){
+    console.log("got some");
+    const buf = Buffer.from(parsedData.data, 'base64');
+    fs.writeFileSync('play.mp3', buf);
+    player.play('play.mp3', (err) => {
+        if (err) {
+            console.log(err);
+        }
+        }
+    );
+  }
+});
