61ae7a4dc6dda79bc02decef51199f9d30c787b3
Matej Holubec <matej.holubec@gmail.com>
2024-03-12 18:47:57 +0100

    fix

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,7 @@ import * as fs from "fs";
 console.log("starting...");
 
 const ws = new WebSocket("ws://192.168.0.112:8085");
-const player = sound({
-    player: 'mpg123'
-});
+const player = sound();
 
 ws.on("open", () => {
   console.log("Connected to server");
@@ -25,8 +23,8 @@ ws.on("message", (data) => {
   if(parsedData.action === 'go'){
     console.log("got some");
     const buf = Buffer.from(parsedData.data, 'base64url');
-    fs.writeFileSync('play.mp3', buf);
-    player.play('play.mp3', (err) => {
+    fs.writeFileSync('play.wav', buf);
+    player.play('play.wav', (err) => {
         if (err) {
             console.log(err);
         }
